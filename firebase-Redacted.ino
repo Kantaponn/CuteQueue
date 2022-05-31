8321886fc04ac8ac4d5063a1ec60b5f4acb45f2a
Kantaponn <78014536+Kantaponn@users.noreply.github.com>
2022-05-31 18:14:43 +0700

    Add timestamp to the file that will be sent

--- firebase-Redacted.ino
+++ firebase-Redacted.ino
@@ -5,6 +5,8 @@
   #include <ESP8266WiFi.h>
 #endif
 #include <Firebase_ESP_Client.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 #define RXD2 16
 #define TXD2 17
@@ -17,14 +19,14 @@
 /*#define WIFI_SSID "HOME WIFI_2.4G"
 #define WIFI_PASSWORD "43567890"
 */
-#define WIFI_SSID "REDACTED"
-#define WIFI_PASSWORD "REDACTED"
+#define WIFI_SSID "Redacted"
+#define WIFI_PASSWORD "Redacted"
 
 // Insert Firebase project API Key
-#define API_KEY "REDACTED"
+#define API_KEY "Redacted"
 
 // Insert RTDB URLefine the RTDB URL */
-#define DATABASE_URL "REDACTED"
+#define DATABASE_URL "Redacted" 
 
 //Define Firebase Data object
 FirebaseData fbdo;
@@ -35,6 +37,15 @@ FirebaseConfig config;
 unsigned long sendDataPrevMillis = 0;
 int count = 0;
 bool signupOK = false;
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+// Variables to save date and time
+String formattedDate;
+String dayStamp;
+String timeStamp;
 
 void firebase_write_reset(int input){
   if (Firebase.ready() && signupOK){
@@ -85,16 +96,32 @@ void setup(){
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
 
+  timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(28800-3600);
   Serial.println("Start Reset");
   firebase_write_reset(1);
   delay(5000);
   firebase_write_reset(0);
   Serial.println("Reset finished");
+  
 }
 
-void firebase_write(int input){
+void firebase_write(int input,String tim){
+  FirebaseJson data;
+  data.set("queue", input);
+  data.set("time", tim);
+  data.set("id", input);
+  data.set("status", "queue details");
+  data.set("remain", 0);
   if (Firebase.ready() && signupOK){
-    if (Firebase.RTDB.setInt(&fbdo, "sensor", input)){
+    //String q=input+'0';
+    //String c = String(input);
+    if (Firebase.RTDB.setJSON(&fbdo, "queueData/"+String(input)+"/" +String(input), &data)){
       Serial.println("PASSED");
       Serial.println("PATH: " + fbdo.dataPath());
       Serial.println("TYPE: " + fbdo.dataType());
@@ -108,15 +135,21 @@ void firebase_write(int input){
 bool last =false;
 int queue=1;
 void loop(){
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+  formattedDate = timeClient.getFormattedDate();
+  int splitT = formattedDate.indexOf("T");
+  timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
+ // Serial.print("HOUR: ");
+  //Serial.println(timeStamp);
    String recieve_string = Serial2.readString();
   //Serial.println(recieve_string[0]);
   if(recieve_string[0]=='1' && last ==false){
-    firebase_write(queue);
+    firebase_write(queue,timeStamp);
     delay(200);
-    firebase_write(0);
     Serial.println(queue);
     queue+=1;
-    
     last=true;
   }
   else if(recieve_string[0]=='0')
